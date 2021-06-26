e9da2433cf0dedb6c3d1520e63d5ccd5d93b6f43
GabrielFNLima <gabrielfnlima@gmail.com>
2021-06-25 23:40:23 -0300

    Init

--- App.js
+++ App.js
@@ -7,9 +7,15 @@ import svgImage from "./bg.svg";
 export default function App() {
   return (
     <View style={styles.container}>
-      
+      {/* colocar o svg e o botao dentro de uma  view e na view colocar o position absolute+9 */}
       <StatusBar style="auto" />
-      <Svg height="60%" width="100%" rotation={180} style={{position:'absolute',bottom:0}}>
+
+      <Svg
+        height="60%"
+        width="100%"
+        rotation={180}
+        style={{ position: "absolute", bottom: 0 }}
+      >
         <Defs>
           <ClipPath id="clip">
             <Pattern
@@ -19,40 +25,47 @@ export default function App() {
               height="1"
             >
               <Path
-                d="M360 99L360 0L1.9845e-05 -3.14722e-05L2.44784e-06 199C4.86195e-06 171.386 22.3858 149 50 149L310 149C337.614 149 360 126.614 360 99Z"
+                d="M450 99L450 0L1.9845e-05 -3.14722e-05L2.44784e-06 199C4.86195e-06 171.386 22.3858 139 100 139L400 139C337.614 139 450 126.614 450 99Z"
                 fill="#FFD200"
               />
             </Pattern>
-            </ClipPath>
+          </ClipPath>
         </Defs>
-              <Path
-                d="M360 99L360 0L1.9845e-05 -3.14722e-05L2.44784e-06 199C4.86195e-06 171.386 22.3858 149 50 149L310 149C337.614 149 360 126.614 360 99Z"
-                fill="#FFD200"
-              />
-            <Image
-              x="0"
-              y="0"
-              width="100%"
-              height="100%"
-              preserveAspectRatio="xMidYMid slice"
-              opacity="1"
-              href={require("./bg-img.png")}
-              clipPath="url(#clip)"
-            />
-        <View
+        <Path
+          d="M450 99L450 0L1.9845e-05 -3.14722e-05L2.44784e-06 199C4.86195e-06 171.386 22.3858 139 100 139L400 139C337.614 139 450 126.614 450 99Z"
+          fill="#FFD200"
+        />
+        <Image
+          x="0"
+          y="0"
+          width="100%"
+          height="100%"
+          preserveAspectRatio="xMidYMid slice"
+          opacity="1"
+          href={require("./bg-img.png")}
+          clipPath="url(#clip)"
+        />
+        
+      </Svg>
+      <View
           style={[
             {
               width: "100%",
-              height: "60%",
+              // height: "60%",
               justifyContent: "center",
               alignItems: "flex-end",
               paddingRight: 43,
-              transform: [{ rotate: "-180deg" }],
+              marginBottom:43,
+              position: "absolute",
+              bottom: 0
             },
           ]}
         >
           <View style={{ flexDirection: "row" }}>
-            <TouchableOpacity onPress={()=>{console.log('press')}}
+            <TouchableOpacity
+              onPress={() => {
+                console.log("press");
+              }}
               style={{
                 width: 100,
                 height: 50,
@@ -78,8 +91,12 @@ export default function App() {
             </TouchableOpacity>
           </View>
         </View>
-      </Svg>
-      <Svg height="60%" width="100%" rotation={0} style={{position:'absolute',top:0}}>
+      <Svg
+        height="60%"
+        width="100%"
+        rotation={0}
+        style={{ position: "absolute", top: 0 }}
+      >
         <Defs>
           <ClipPath id="clip">
             <Pattern
@@ -89,26 +106,26 @@ export default function App() {
               height="1"
             >
               <Path
-                d="M360 99L360 0L1.9845e-05 -3.14722e-05L2.44784e-06 199C4.86195e-06 171.386 22.3858 149 50 149L310 149C337.614 149 360 126.614 360 99Z"
+                d="M360 99L360 0L1.9845e-05 -3.14722e-05L2.44784e-06 199C4.86195e-06 171.386 22.3858 139 50 139L400 139C337.614 139 360 126.614 360 99Z"
                 fill="#FFD200"
               />
             </Pattern>
-            </ClipPath>
+          </ClipPath>
         </Defs>
-              <Path
-                d="M360 99L360 0L1.9845e-05 -3.14722e-05L2.44784e-06 199C4.86195e-06 171.386 22.3858 149 50 149L310 149C337.614 149 360 126.614 360 99Z"
-                fill="#FFD200"
-              />
-            <Image
-              x="0"
-              y="0"
-              width="100%"
-              height="100%"
-              preserveAspectRatio="xMidYMid slice"
-              opacity="1"
-              href={require("./bg-img.png")}
-              clipPath="url(#clip)"
-            />
+        <Path
+          d="M360 99L360 0L1.9845e-05 -3.14722e-05L2.44784e-06 199C4.86195e-06 171.386 22.3858 139 50 139L400 139C337.614 139 360 126.614 360 99Z"
+          fill="#FFD200"
+        />
+        <Image
+          x="0"
+          y="0"
+          width="100%"
+          height="100%"
+          preserveAspectRatio="xMidYMid slice"
+          opacity="1"
+          href={require("./bg-img.png")}
+          clipPath="url(#clip)"
+        />
         <View
           style={{
             width: "100%",
@@ -121,7 +138,22 @@ export default function App() {
           <Text style={{ fontSize: 16 }}>Access account</Text>
         </View>
       </Svg>
-    
+      {/* <TouchableOpacity
+        onPress={() => {
+          console.log("press");
+        }}
+        style={{
+          width: 100,
+          height: 50,
+          backgroundColor: "#fff",
+          justifyContent: "center",
+          alignItems: "center",
+          borderRadius: 15,
+          marginRight: 15,
+        }}
+      >
+        <Text style={{ fontSize: 16 }}>Next2</Text>
+      </TouchableOpacity> */}
     </View>
   );
 }
